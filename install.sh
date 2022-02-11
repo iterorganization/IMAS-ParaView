9ad072849a41854f2d69204d0112f71491283fc2
Panchumarti Jaswant EXT <Jaswant.Panchumarti@iter.org>
2022-02-11 21:57:48 +0100

    Plugin goes into bin/plugins.

--- install.sh
+++ install.sh
@@ -2,17 +2,35 @@
 # For example:
 # 	$ source install.sh /home/ITER/panchuj/work/Debug/install
 # vtkggdtools will be installed in /home/ITER/panchuj/work/Debug/install/lib/pythonx.y/site-packages
+# the paraview plugin will go in /home/ITER/panchuj/work/Debug/install/bin/plugins
 
-install_dir=$1
 python_majmin_ver=$(python -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))')
-pip install --prefix=${install_dir} --no-deps .
+install_dir=$1
+if [ ! -d ${install_dir} ]; then
+  install_dir=$(pwd)
+else
+  install_dir=$(cd ${install_dir}; pwd)
+fi
 
-echo "Installed into ${install_dir}/lib/python${python_majmin_ver}/site-packages/vtkggdtools"
+library_install_dir="${install_dir}/lib/python${python_majmin_ver}/site-packages/vtkggdtools"
+plugin_install_dir="${install_dir}/bin/plugins"
 
-export PYTHONPATH=${PYTHONPATH}:${install_dir}/lib/python${python_majmin_ver}/site-packages
+pip install --prefix="${install_dir}" --no-deps .
+
+echo "Installed into ${library_install_dir}"
+
+[ -d ${plugin_install_dir} ] || mkdir -p ${plugin_install_dir}
+mv "${library_install_dir}/plugins/VTKGGDTools.py" "${plugin_install_dir}/"
+
+export PYTHONPATH=${PYTHONPATH}:"${install_dir}/lib/python${python_majmin_ver}/site-packages"
 export PYTHONPATH=$(python3 -c 'import os; from collections import OrderedDict; \
     l=os.environ["PYTHONPATH"].split(":"); print(":".join(OrderedDict.fromkeys(l)))' )
 
-export PV_PLUGIN_PATH=${PV_PLUGIN_PATH}:${install_dir}/lib/python${python_majmin_ver}/site-packages/vtkggdtools/plugins
+export PV_PLUGIN_PATH=${PV_PLUGIN_PATH}:"${plugin_install_dir}"
 export PV_PLUGIN_PATH=$(python3 -c 'import os; from collections import OrderedDict; \
     l=os.environ["PV_PLUGIN_PATH"].split(":"); print(":".join(OrderedDict.fromkeys(l)))' )
+
+unset python_majmin_ver
+unset library_install_dir
+unset plugin_install_dir
+unset install_dir
