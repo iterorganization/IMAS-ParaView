8555fc4d0478de7cbb53228544cdaa72247b50a5
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-08-14 14:44:10 +0200

    add pytest

--- vtkggdtools/tests/test_read_ps.py
+++ vtkggdtools/tests/test_read_ps.py
@@ -1,3 +1,4 @@
+import imaspy
 import pytest
 from vtkmodules.vtkCommonCore import vtkPoints
 
@@ -16,14 +17,36 @@ def test_read_plasma_state(ids_name, dummy_ids):
     elif ids_name == "runaway_electrons":
         pytest.skip(reason="runaway_electrons is not implemented")
 
-    ids = dummy_ids
     subset_idx = 0
     space_idx = 0
-    grid_ggd = get_first_grid(ids)
+    grid_ggd = get_first_grid(dummy_ids)
     points = vtkPoints()
     fill_vtk_points(grid_ggd, space_idx, points, ids_name)
     ugrid = convert_grid_subset_geometry_to_unstructured_grid(
         grid_ggd, subset_idx, points
     )
-    ps_reader = PlasmaStateReader(ids)
+    ps_reader = PlasmaStateReader(dummy_ids)
     ps_reader.read_plasma_state(subset_idx, ugrid)
+
+
+def test_create_name_with_units():
+    ids = imaspy.IDSFactory().new("edge_sources")
+    ps_reader = PlasmaStateReader(ids)
+
+    # Resize relevant structures
+    ids.source.resize(1)
+    ids.source[0].ggd.resize(1)
+    ids.source[0].ggd[0].ion.resize(1)
+    ids.source[0].ggd[0].ion[0].state.resize(1)
+    array = ids.source[0].ggd[0].ion[0].state[0].energy
+
+    name = ps_reader._create_name_with_units(array)
+    assert name == "source ion state energy [W.m^-3]"
+
+    # Set identifier name and labels
+    ids.source[0].identifier.name = "Charge exchange"
+    ids.source[0].ggd[0].ion[0].label = "Ar"
+    ids.source[0].ggd[0].ion[0].state[0].label = "Ar+14"
+
+    name = ps_reader._create_name_with_units(array)
+    assert name == "source (Charge exchange) ion (Ar) state (Ar+14) energy [W.m^-3]"
