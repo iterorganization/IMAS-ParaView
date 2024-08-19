546d0e38b76febbfe510cffdaebf76a61afd7913
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-08-19 16:45:03 +0200

    Reroute imaspy logs by replacing its handler to vtk handler

--- vtkggdtools/__init__.py
+++ vtkggdtools/__init__.py
@@ -6,9 +6,16 @@ from . import _version
 
 __version__ = _version.get_versions()["version"]
 
-logger = logging.getLogger(__name__)
-
-# Add VTK handler
+# Setup logger for GGDVTK module
+logger = logging.getLogger("ggdvtk")
 handler = VTKHandler()
 logger.addHandler(handler)
 logger.setLevel(handler.get_level())
+
+# Remove imaspy handler and add the VTK handler to make sure IMASPy logs are correctly
+# displayed in Paraview's logs
+imaspy_logger = logging.getLogger("imaspy")
+if len(imaspy_logger.handlers) == 1:
+    imaspy_logger.removeHandler(imaspy_logger.handlers[0])
+imaspy_logger.addHandler(handler)
+imaspy_logger.setLevel(handler.get_level())
