e3a04668190bc1674adc929152acdac8e0a105b5
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-08-20 16:23:09 +0200

    changes to docstrings

--- vtkggdtools/_ids_util.py
+++ vtkggdtools/_ids_util.py
@@ -5,18 +5,16 @@ from imaspy.ids_struct_array import IDSStructArray
 def _recursive_ggd_path_search(
     quantity_metadata, scalar_array_paths, vector_array_paths
 ):
-    """Recursively searches through the IDS node for scalar (real & complex) and
-    vector arrays, and appends the paths of these to the scalar_array_paths and
-    vector_array_paths respectively.
+    """Recursively searches through the metadata of an IDS  node for scalar GGD arrays
+    (real & complex) and vector GGD arrays (regular and rphiz), and appends the paths of
+    these to the scalar_array_paths and vector_array_paths respectively.
 
     Args:
-        quantity: The IDS node to search from
-        scalar_array_list: The GGD scalar arrays (real & complex)
-        vector_array_list: The GGD vector arrays
-        get_empty_arrays (bool): Whether to return empty arrays
+        quantity_metadata: The metadata of an IDS node
+        scalar_array_paths: The IDSPaths of GGD scalar arrays (real & complex)
+        vector_array_paths: The IDSPaths of GGD vector arrays (regular and rphiz)
     """
     for subquantity_metadata in quantity_metadata:
-        # If subquantity is a struct array
         if subquantity_metadata.data_type == IDSDataType.STRUCT_ARRAY:
             # Get scalar and complex scalar array quantities
             if subquantity_metadata.structure_reference in [
@@ -32,7 +30,6 @@ def _recursive_ggd_path_search(
                 "generic_grid_vector_components_rzphi",
             ]:
                 vector_array_paths.append(subquantity_metadata.path)
-            # Recursively search
             else:
                 _recursive_ggd_path_search(
                     subquantity_metadata,
@@ -40,7 +37,6 @@ def _recursive_ggd_path_search(
                     vector_array_paths,
                 )
 
-        # If subquantity is a structure
         elif subquantity_metadata.data_type == IDSDataType.STRUCTURE:
             # Skip "grid" quantity, this can occur if the grid is stored within the
             # GGD. e.g. in distribution_sources distributions IDSs
@@ -60,22 +56,20 @@ def _get_nodes_from_path(node, path):
         path: An IDSPath to traverse.
 
     Returns:
-        list: A list of nodes obtained from the specified path.
+        A list of nodes obtained from the specified path.
     """
     return list(_iter_nodes_from_path(node, path.parts))
 
 
 def _iter_nodes_from_path(node, path_parts):
-    """Recursively iterate through nodes in a hierarchical structure based on path
-    parts.
+    """Recursively iterate through nodes in an IDS mpde based on path parts.
 
     Args:
         node: The current node being traversed.
-        path_parts: A list of IDSPath segments (parts) that guide the traversal through
-        the structure.
+        path_parts: A list of IDSPath segments (parts).
 
     Yields:
-        node: The next node in the structure corresponding to the current path part.
+        The next node in the structure corresponding to the current path part.
     """
     child_node = node[path_parts[0]]
     if len(path_parts) == 1:
