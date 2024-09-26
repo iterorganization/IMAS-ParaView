372fb65f632ee758478b6ed884ecf42a7aa39b74
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-09-26 14:14:39 +0200

    test CLI with memory backend instead

--- vtkggdtools/tests/test_cli.py
+++ vtkggdtools/tests/test_cli.py
@@ -1,3 +1,4 @@
+import imaspy
 from click.testing import CliRunner
 
 from vtkggdtools.cli import convert_ggd_to_vtk, print_version
@@ -9,12 +10,16 @@ def test_version():
     assert result.exit_code == 0
 
 
-def test_ggd2vtk(tmp_path):
-    uri = "imas:hdf5?path=vtkggdtools/tests/test_ids/"
+def test_ggd2vtk(tmp_path, dummy_ids):
+    uri = f"imas:hdf5?path={tmp_path}/testdb"
+    with imaspy.DBEntry(uri, "w") as dbentry:
+        dbentry.put(dummy_ids)
+
     runner = CliRunner()
-    output_path = tmp_path / "test"
+    file_name = "test"
+    output_path = tmp_path / file_name
+    ids_name = dummy_ids.metadata.name
     output_str = str(output_path)
-    ids_name = "edge_profiles"
     occurrence = "0"
     args = [uri, ids_name, output_str, occurrence]
     result = runner.invoke(convert_ggd_to_vtk, args)
@@ -23,6 +28,11 @@ def test_ggd2vtk(tmp_path):
     # Check if vtpc file and the directory containing vtu files exists
     assert output_path.with_suffix(".vtpc").exists()
     assert output_path.is_dir()
-    for i in range(3):
-        vtu_file = output_path / f"test_{i}_0.vtu"
+
+    if ids_name == "wall":
+        vtu_file = output_path / f"{file_name}_0_0.vtu"
         assert vtu_file.exists()
+    else:
+        for i in range(3):
+            vtu_file = output_path / f"{file_name}_{i}_0.vtu"
+            assert vtu_file.exists()
