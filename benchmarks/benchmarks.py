28505810881f59d414ba19fe45fd0e44954baef5
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-09-27 16:21:05 +0200

    change benchmark

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -5,18 +5,6 @@ import imaspy
 from vtkggdtools.convert import ggd_to_vtk
 from vtkggdtools.tests.fill_ggd import fill_ids
 
-HDF5 = "HDF5"
-MDSPLUS = "MDSplus"
-MEMORY = "memory"
-ASCII = "ASCII"
-
-all_backends = [
-    HDF5,
-    MDSPLUS,
-    MEMORY,
-    ASCII,
-]
-
 
 def create_uri(backend):
     path = Path.cwd() / f"DB-{backend}"
@@ -24,26 +12,47 @@ def create_uri(backend):
 
 
 class Convert:
-    params = all_backends
-
-    def setup(self, backend):
+    def setup(self):
         self.edge_profiles = imaspy.IDSFactory().edge_profiles()
-        fill_ids(self.edge_profiles, N=10)
-        uri = create_uri(backend)
-        with imaspy.DBEntry(uri, "w") as dbentry:
-            dbentry.put(self.edge_profiles)
+        fill_ids(self.edge_profiles, N=3)
 
-    def time_convert(self, backend):
+    def time_convert(self):
         ggd_to_vtk(self.edge_profiles, 0)
 
-    def time_load_and_convert(self, backend):
-        uri = create_uri(backend)
-        entry = imaspy.DBEntry(uri, "r")
+
+class ConvertHDF5:
+    def setup(self):
+        es = imaspy.IDSFactory().edge_profiles()
+        fill_ids(es, N=3)
+        self.uri = create_uri("hdf5")
+        with imaspy.DBEntry(self.uri, "w") as dbentry:
+            dbentry.put(es)
+
+    def time_load_and_convert(self):
+        entry = imaspy.DBEntry(self.uri, "r")
         ids = entry.get("edge_profiles", lazy=False, autoconvert=False)
         ggd_to_vtk(ids, 0)
 
-    def time_load_and_convert_lazy(self, backend):
-        uri = create_uri(backend)
-        entry = imaspy.DBEntry(uri, "r")
+    def time_load_and_convert_lazy(self):
+        entry = imaspy.DBEntry(self.uri, "r")
+        ids = entry.get("edge_profiles", lazy=True, autoconvert=False)
+        ggd_to_vtk(ids, 0)
+
+
+class ConvertMDSPlus:
+    def setup(self):
+        es = imaspy.IDSFactory().edge_profiles()
+        fill_ids(es, N=3)
+        self.uri = create_uri("mdsplus")
+        with imaspy.DBEntry(self.uri, "w") as dbentry:
+            dbentry.put(es)
+
+    def time_load_and_convert(self):
+        entry = imaspy.DBEntry(self.uri, "r")
+        ids = entry.get("edge_profiles", lazy=False, autoconvert=False)
+        ggd_to_vtk(ids, 0)
+
+    def time_load_and_convert_lazy(self):
+        entry = imaspy.DBEntry(self.uri, "r")
         ids = entry.get("edge_profiles", lazy=True, autoconvert=False)
         ggd_to_vtk(ids, 0)
