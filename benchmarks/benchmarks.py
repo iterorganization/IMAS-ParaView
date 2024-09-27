326f5227735487267a923cf1361a4394de1aaeed
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-09-27 15:39:13 +0200

    add more benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,14 +1,50 @@
+from pathlib import Path
+
 import imaspy
 
 from vtkggdtools.convert import ggd_to_vtk
 from vtkggdtools.tests.fill_ggd import fill_ids
 
+HDF5 = "HDF5"
+MDSPLUS = "MDSplus"
+MEMORY = "memory"
+ASCII = "ASCII"
+
+all_backends = [
+    HDF5,
+    MDSPLUS,
+    MEMORY,
+    ASCII,
+]
+
+
+def create_uri(backend):
+    path = Path.cwd() / f"DB-{backend}"
+    return f"imas:{backend.lower()}?path={path}"
+
 
 class Convert:
+    params = all_backends
 
-    def setup(self):
+    def setup(self, backend):
         self.edge_profiles = imaspy.IDSFactory().edge_profiles()
-        fill_ids(self.edge_profiles, N=25)
+        fill_ids(self.edge_profiles, N=50)
+        uri = create_uri(backend)
+        with imaspy.DBEntry(uri, "w") as dbentry:
+            dbentry.put(self.edge_profiles)
 
-    def time_convert(self):
+    def time_convert(self, backend):
         ggd_to_vtk(self.edge_profiles, 0)
+
+    def time_load_and_convert(self, backend):
+        uri = create_uri(backend)
+        entry = imaspy.DBEntry(uri, "r")
+        ids = entry.get("edge_profiles", lazy=False, autoconvert=False)
+        ggd_to_vtk(ids, 0)
+
+    def time_load_and_convert_lazy(self, backend):
+        path = Path.cwd() / f"DB-{backend}"
+        uri = f"imas:{backend.lower()}?path={path}"
+        entry = imaspy.DBEntry(uri, "r")
+        ids = entry.get("edge_profiles", lazy=True, autoconvert=False)
+        ggd_to_vtk(ids, 0)
