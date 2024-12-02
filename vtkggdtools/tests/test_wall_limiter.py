2f4d4b769807e88218c8e099bc24e2edb8554430
Sebbe Blokhuizen <sblokhuizen@ignitioncomputing.com>
2024-12-02 15:20:07 +0100

    use dynamic naming

--- vtkggdtools/tests/test_wall_limiter.py
+++ vtkggdtools/tests/test_wall_limiter.py
@@ -8,22 +8,29 @@ def test_load_limiters():
     """Test if limiters are loaded in the VTK Multiblock Dataset."""
     reader = IMASPyWallLimiterReader()
     entry = DBEntry(
-            "imas:hdf5?path=/work/imas/shared/imasdb/ITER_MACHINE_DESCRIPTION/3/116000/5/", "r"
+        "imas:hdf5?path=/work/imas/shared/imasdb/ITER_MACHINE_DESCRIPTION/3/116000/5/",
+        "r",
     )
     ids = entry.get("wall", lazy=True, autoconvert=False)
-    reader._ids = ids 
+    reader._ids = ids
     reader.setup_ids()
-    
+    description_name = ids.description_2d[0].type.name
+    limiter_name = ids.description_2d[0].limiter.type.name
+    unit_name1 = ids.description_2d[0].limiter.unit[0].name
+    unit_name2 = ids.description_2d[0].limiter.unit[0].name
+
     output = vtkMultiBlockDataSet()
     assert output.GetNumberOfBlocks() == 0
 
     output = vtkMultiBlockDataSet()
-    reader._selected = ["Wall description at operating temperature (100 C) / Divertor and FW / First Wall"] 
+    reader._selected = [f"{description_name} / {limiter_name} / {unit_name1}"]
     reader._load_limiters(output)
     assert output.GetNumberOfBlocks() == 1
-    
+
     output = vtkMultiBlockDataSet()
-    reader._selected = ["Wall description at operating temperature (100 C) / Divertor and FW / First Wall", 
-    "Wall description at operating temperature (100 C) / Divertor and FW / Divertor"] 
+    reader._selected = [
+        f"{description_name} / {limiter_name} / {unit_name1}",
+        f"{description_name} / {limiter_name} / {unit_name2}",
+    ]
     reader._load_limiters(output)
     assert output.GetNumberOfBlocks() == 2
