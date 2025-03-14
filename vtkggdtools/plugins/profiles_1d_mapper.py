dd875d21cec1fbaad79a16be3f545ee2eeaf6ddc
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-03-14 14:51:47 +0100

    Add docstrings

--- vtkggdtools/plugins/profiles_1d_mapper.py
+++ vtkggdtools/plugins/profiles_1d_mapper.py
@@ -1,6 +1,8 @@
+import logging
+
 import numpy as np
 import vtk
-from paraview.util.vtkAlgorithm import smdomain, smproperty, smproxy
+from paraview.util.vtkAlgorithm import smdomain, smhint, smproperty, smproxy
 from vtkmodules.numpy_interface import dataset_adapter as dsa
 from vtkmodules.util.vtkAlgorithm import VTKPythonAlgorithmBase
 from vtkmodules.vtkCommonDataModel import vtkPartitionedDataSetCollection, vtkTable
@@ -11,15 +13,25 @@ from vtkggdtools.paraview_support.servermanager_tools import (
 )
 from vtkggdtools.progress import Progress
 
+logger = logging.getLogger("vtkggdtools")
 
-@smproxy.filter(label="test_filter")
+
+@smproxy.filter(label="1D Profiles Mapper")
+@smhint.xml("""<ShowInMenu category="IMAS Tools" />""")
 @smproperty.input(name="1D Profile", port_index=1)
 @smdomain.datatype(dataTypes=["vtkTable"], composite_data_supported=False)
 @smproperty.input(name="Psi Grid", port_index=0)
 @smdomain.datatype(
     dataTypes=["vtkPartitionedDataSetCollection"], composite_data_supported=True
 )
-class ExampleTwoInputFilter(VTKPythonAlgorithmBase):
+class Profiles1DMapper(VTKPythonAlgorithmBase):
+    """Filter that maps 1d profiles onto R,Z Rectangular coordinates. This filter takes
+    2 inputs:
+    - A poloidal flux (Psi) from a GGD. i.e. from GGD Reader/JOREK Reader (input 0)
+    - A 1D profile from the 1DProfilesReader. (input 1)
+    The 1DProfilesReader must output at least the Psi grid coordinate, and can output
+    any number of 1D profiles."""
+
     def __init__(self):
         VTKPythonAlgorithmBase.__init__(
             self,
@@ -42,22 +54,38 @@ class ExampleTwoInputFilter(VTKPythonAlgorithmBase):
             vtkPartitionedDataSetCollection.GetData(inInfoVec[0], 0)
         )
         input1 = dsa.WrapDataObject(vtkTable.GetData(inInfoVec[1], 0))
-
-        psi_grid = input0.PointData["Psi [Wb]"].GetArrays()[0]
-        psi_profiles = input1.RowData["Grid Psi"]
         self._selectable = input1.RowData.keys()
+
+        psi = input0.PointData["Psi [Wb]"]
+
+        if isinstance(psi, dsa.VTKNoneArray):
+            logger.warning(
+                "The GGD Reader should output a poloidal flux GGD. Please select 'Psi' "
+                "in the attribute selector window."
+            )
+            return 1
+        psi_grid = psi.GetArrays()[0]
+        psi_profiles = input1.RowData["Grid Psi"]
 
         output = dsa.WrapDataObject(vtkPartitionedDataSetCollection.GetData(outInfo))
         output.ShallowCopy(input0.VTKObject)
 
-        # Create progress object to advance Paraview progress bar
         progress = Progress(self.UpdateProgress)
         for profile_name in self._selected:
             profile = input1.RowData[profile_name]
 
-            piecewise_function = vtk.vtkPiecewiseFunction()
+            if isinstance(psi_profiles, dsa.VTKNoneArray):
+                logger.warning(
+                    "The 1DProfilesReader should output a poloidal flux grid. Please"
+                    " select 'Grid Psi' in the attribute selector window of the "
+                    "1DProfilesReader."
+                )
+                return 1
+
             # Interpolate to zero outside the core profiles range
+            piecewise_function = vtk.vtkPiecewiseFunction()
             piecewise_function.ClampingOff()
+
             for i in range(psi_profiles.GetNumberOfTuples()):
                 piecewise_function.AddPoint(
                     psi_profiles.GetTuple1(i), profile.GetTuple1(i)
@@ -74,12 +102,12 @@ class ExampleTwoInputFilter(VTKPythonAlgorithmBase):
         return 1
 
     @arrayselectiondomain(
-        property_name="AttributeArray",
-        name="AttributeArraySelector",
-        label="Select attribute Arrays",
+        property_name="Profile1DArray",
+        name="Profile1DSelector",
+        label="Select 1D Profiles",
     )
-    def P12_SetAttributeArray(self, array, status):
-        """Select all or a subset of available GGD arrays to load."""
+    def SetProfile1D(self, array, status):
+        """Select all or a subset of available 1d_profiles to load."""
         # Add an array to selected list
         if status == 1 and array not in self._selected:
             self._selected.append(array)
@@ -91,16 +119,16 @@ class ExampleTwoInputFilter(VTKPythonAlgorithmBase):
             self.Modified()
 
     @arrayselectionstringvector(
-        property_name="AttributeArray", attribute_name="Attribute"
+        property_name="Profile1DArray", attribute_name="Profile1D"
     )
-    def _AttributeArraySelector(self):
+    def _Profile1DArraySelector(self):
         pass
 
-    def GetNumberOfAttributeArrays(self):
+    def GetNumberOfProfile1DArrays(self):
         return len(self._selectable)
 
-    def GetAttributeArrayName(self, idx) -> str:
+    def GetProfile1DArrayName(self, idx) -> str:
         return self._selectable[idx]
 
-    def GetAttributeArrayStatus(self, *args):
+    def GetProfile1DArrayStatus(self, *args):
         return 1
