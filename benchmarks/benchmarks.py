8e860cc2b03d484e6e5b847bb6c704e5edf36ae8
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-05-07 16:16:31 +0200

    skip hdf5/mdsplus benchmarks and add NetCDF benchmark

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import imas
+from asv_runner.benchmarks.mark import skip_benchmark
 
 from imas_paraview.convert import Converter
 from imas_paraview.tests.fill_ggd import fill_ids
@@ -23,11 +24,11 @@ class Convert:
         converter.ggd_to_vtk(time_idx=0)
 
 
-class ConvertHDF5:
+class ConvertNetCDF:
     def setup(self):
         es = imas.IDSFactory().edge_profiles()
         fill_ids(es, grid_size=SIZE_GRID)
-        self.uri = create_uri("hdf5")
+        self.uri = Path.cwd() / "DB-netcdf.nc"
         with imas.DBEntry(self.uri, "w") as dbentry:
             dbentry.put(es)
 
@@ -44,9 +45,31 @@ class ConvertHDF5:
         converter.ggd_to_vtk(time_idx=0)
 
 
-class ConvertMDSPlus:
-    timeout = 200
+# TODO: HDF5 and MDSPlus benchmarks are skipped as IMAS-Core is not yet available
+class ConvertHDF5:
+    def setup(self):
+        es = imas.IDSFactory().edge_profiles()
+        fill_ids(es, grid_size=SIZE_GRID)
+        self.uri = create_uri("hdf5")
+        with imas.DBEntry(self.uri, "w") as dbentry:
+            dbentry.put(es)
 
+    @skip_benchmark
+    def time_load_and_convert(self):
+        entry = imas.DBEntry(self.uri, "r")
+        ids = entry.get("edge_profiles", lazy=False, autoconvert=False)
+        converter = Converter(ids)
+        converter.ggd_to_vtk(time_idx=0)
+
+    @skip_benchmark
+    def time_load_and_convert_lazy(self):
+        entry = imas.DBEntry(self.uri, "r")
+        ids = entry.get("edge_profiles", lazy=True, autoconvert=False)
+        converter = Converter(ids)
+        converter.ggd_to_vtk(time_idx=0)
+
+
+class ConvertMDSPlus:
     def setup(self):
         es = imas.IDSFactory().edge_profiles()
         fill_ids(es, grid_size=SIZE_GRID)
@@ -54,12 +77,14 @@ class ConvertMDSPlus:
         with imas.DBEntry(self.uri, "w") as dbentry:
             dbentry.put(es)
 
+    @skip_benchmark
     def time_load_and_convert(self):
         entry = imas.DBEntry(self.uri, "r")
         ids = entry.get("edge_profiles", lazy=False, autoconvert=False)
         converter = Converter(ids)
         converter.ggd_to_vtk(time_idx=0)
 
+    @skip_benchmark
     def time_load_and_convert_lazy(self):
         entry = imas.DBEntry(self.uri, "r")
         ids = entry.get("edge_profiles", lazy=True, autoconvert=False)
