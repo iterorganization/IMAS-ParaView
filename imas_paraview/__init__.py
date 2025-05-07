0b790cd8a5374361c66f848d6ab7a4188ff0e6d1
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-05-07 16:40:28 +0200

    update logger name IMAS-Python to correct name

--- imas_paraview/__init__.py
+++ imas_paraview/__init__.py
@@ -17,17 +17,17 @@ logger.setLevel(handler.get_level())
 
 # Remove imas handler and add the VTK handler to make sure IMAS-Python logs are
 # correctly displayed in Paraview's logs
-imas_logger = logging.getLogger("imas_python")
+imas_logger = logging.getLogger("imas")
 if len(imas_logger.handlers) == 1:
     imas_logger.removeHandler(imas_logger.handlers[0])
 imas_logger.addHandler(handler)
 imas_logger.setLevel(handler.get_level())
 
-# Check imas version
+# Check IMAS-Python version
 imas_version = imas.__version__
 required_imas_python_version = "2.0.0"
 if version.parse(imas_version) < version.parse(required_imas_python_version):
     logger.warning(
-        f"imas version {imas_version} is lower than the recommended version "
+        f"IMAS-Python version {imas_version} is lower than the recommended version "
         f"{required_imas_python_version}. Some features might not work as expected."
     )
