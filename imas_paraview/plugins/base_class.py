7946a2a73e64f28e4488773564334f3a95c8bd49
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-06-18 15:47:31 +0200

    revert heterogeneous update

--- imas_paraview/plugins/base_class.py
+++ imas_paraview/plugins/base_class.py
@@ -517,9 +517,8 @@ class GGDVTKPluginBase(VTKPythonAlgorithmBase, ABC):
                 self._ids.ids_properties.homogeneous_time
                 == imas.ids_defs.IDS_TIME_MODE_HETEROGENEOUS
             ):
-                logger.warning(
-                    "Heterogeneous IDSs are currently not supported. The global time array will be used instead."
-                )
+                logger.error("Heterogeneous IDSs are currently not supported.")
+                return 1
             self._time_steps = self._ids.time
 
             # Pass time steps to Paraview
