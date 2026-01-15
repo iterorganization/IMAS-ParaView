04002677adf897037ccd704e9f44000f7c47f6ed
Sebbe Blokhuizen <sblokhuizen@ignitioncomputing.com>
2026-01-15 15:50:43 +0100

    add annulus cross-section

--- imas_paraview/plugins/coils_non_axisymmetric.py
+++ imas_paraview/plugins/coils_non_axisymmetric.py
@@ -5,25 +5,19 @@ import logging
 
 import numpy as np
 from paraview.util.vtkAlgorithm import smhint, smproxy
-from vtkmodules.util.numpy_support import numpy_to_vtk
-from vtkmodules.vtkCommonCore import vtkPoints
-from vtkmodules.vtkCommonDataModel import (
-    vtkCellArray,
-    vtkMultiBlockDataSet,
-    vtkPolyData,
-)
+from vtkmodules.vtkCommonDataModel import vtkMultiBlockDataSet
+from vtkmodules.vtkFiltersCore import vtkAppendPolyData, vtkTubeFilter
 
 from imas_paraview.paraview_support.servermanager_tools import intvector, propertygroup
 from imas_paraview.plugins.base_class import GGDVTKPluginBase
-from imas_paraview.util import pol_to_cart
+from imas_paraview.util import points_to_vtkpoly, pol_to_cart
 
 logger = logging.getLogger("imas_paraview")
 
 SUPPORTED_IDS_NAMES = ["coils_non_axisymmetric"]
 
 # TODO: add tests
-# TODO: add docs
-# TODO: implement cross section
+# TODO: add docs / docstrings
 
 
 @smproxy.source(label="Non-Axisymmetric Coils Reader")
@@ -33,14 +27,25 @@ class CoilsNonAxisymmetricReader(GGDVTKPluginBase):
         super().__init__("vtkMultiBlockDataSet", SUPPORTED_IDS_NAMES)
         self.selectable_map = {}
         self.resolution = 10
+        self.cs_resolution = 10
 
     @intvector(label="Resolution", name="resolution", default_values=10)
-    def P99_SetResolution(self, val):
-        """Sets the number of points for 'arcs_of_circle' geometry type, if it is
-        available in the loaded IDS."""
+    def P98_SetResolution(self, val):
+        """Sets the number of points for interpolating the 'arcs_of_circle' and
+        'circle' geometrical element type, if they are available in the loaded IDS."""
         self._update_property("resolution", val)
 
-    @propertygroup("Non-Axisymmetric Coils Reader Settings", ["resolution"])
+    @intvector(
+        label="Cross-sectional Resolution", name="cs_resolution", default_values=10
+    )
+    def P99_SetCrossSectionalResolution(self, val):
+        """Sets the number of points for interpolating the 'annulus' cross-section, if
+        it is available in the loaded IDS."""
+        self._update_property("cs_resolution", val)
+
+    @propertygroup(
+        "Non-Axisymmetric Coils Reader Settings", ["resolution", "cs_resolution"]
+    )
     def PG3_CoilsNonAxisymmetricReaderGroup(self):
         """Dummy function to define a PropertyGroup."""
 
@@ -67,11 +72,11 @@ class CoilsNonAxisymmetricReader(GGDVTKPluginBase):
             if not coil_name:
                 coil_name = f"coil {i}"
                 logger.warning(
-                    "Non-axisymmetric coil without name found. Using %s", coil_name
+                    "Non-axisymmetric coil without name found. Using %r", coil_name
                 )
 
             if len(coil.conductor) == 0:
-                logger.warning("%s has no conductors, skipping it.", coil_name)
+                logger.warning("%r has no conductors, skipping it.", coil_name)
                 continue
 
             has_elements = False
@@ -82,7 +87,7 @@ class CoilsNonAxisymmetricReader(GGDVTKPluginBase):
 
             if not has_elements:
                 logger.warning(
-                    "%s has no elements in any conductor, skipping it.", coil_name
+                    "%r has no elements in any conductor, skipping it.", coil_name
                 )
                 continue
 
@@ -93,70 +98,59 @@ class CoilsNonAxisymmetricReader(GGDVTKPluginBase):
         block_index = 0
         for coil_name in self._selected:
             coil = self.selectable_map[coil_name]
-            for conductor_idx, conductor in enumerate(coil.conductor):
-                elements = conductor.elements
-                conductor_points = []
-                lines = vtkCellArray()
+            logger.info("Loading non-axisymmetric coil %r...", coil_name)
 
-                if len(elements.types) == 0:
+            for conductor in coil.conductor:
+                if len(conductor.elements.types) == 0:
                     logger.warning(
-                        "elements of conductor %d does not have types, skipping",
-                        conductor_idx,
+                        "The geometrical elements of the conductor do not have a type, "
+                        "this conductor will be skipped",
                     )
                     continue
 
-                point_id = 0
-                num_elements_added = 0
-
-                for elem_idx, elem_type in enumerate(elements.types):
-                    elem_points = np.array([])
-
-                    if elem_type == 1:
-                        elem_points = self._create_line_segment(elements, elem_idx)
-                    elif elem_type == 2:
-                        elem_points = self._create_arc_of_circle(elements, elem_idx)
-                    elif elem_type == 3:
-                        elem_points = self._create_circle(elements, elem_idx)
-                    else:
-                        logger.warning(
-                            "%s conductor %d element %d has unsupported element "
-                            "type %d, skipping",
-                            coil_name,
-                            conductor_idx,
-                            elem_idx,
-                            elem_type,
-                        )
-
-                    if len(elem_points) != 0:
-                        conductor_points.append(elem_points)
-                        num_pts = len(elem_points)
-                        lines.InsertNextCell(num_pts)
-                        for i in range(num_pts):
-                            lines.InsertCellPoint(point_id + i)
-                        point_id += num_pts
-                        num_elements_added += 1
-
-                if not conductor_points:
-                    continue
-
-                vtk_pts = vtkPoints()
-                vtk_pts.SetData(numpy_to_vtk(np.vstack(conductor_points)))
-                poly = vtkPolyData()
-                poly.SetPoints(vtk_pts)
-                poly.SetLines(lines)
-                output.SetBlock(block_index, poly)
+                conductor_vtk_geometry = self.create_conductor_geometry(conductor)
+                output.SetBlock(block_index, conductor_vtk_geometry)
                 block_index += 1
-                logger.info(
-                    "Loaded conductor %d from coil %r with %d element(s)",
-                    conductor_idx,
-                    coil_name,
-                    num_elements_added,
-                )
+
             logger.info(
-                "Loaded non-axisymmetric coil %s with %d conductor(s)",
+                "Loaded non-axisymmetric coil %r with %d conductor(s)",
                 coil_name,
                 len(coil.conductor),
             )
+
+    def create_conductor_geometry(self, conductor):
+        elements = conductor.elements
+        conductor_block = vtkAppendPolyData()
+
+        for elem_idx, elem_type in enumerate(elements.types):
+            if elem_type == 1:  # Line segment
+                elem_points = self._create_line_segment(elements, elem_idx)
+            elif elem_type == 2:  # Arc of a circle
+                elem_points = self._create_circular_geometry(
+                    elements, elem_idx, is_full_circle=False
+                )
+            elif elem_type == 3:  # Full circle
+                elem_points = self._create_circular_geometry(
+                    elements, elem_idx, is_full_circle=True
+                )
+            else:
+                logger.warning(
+                    "Conductor element %d has unsupported element type %d, skipping",
+                    elem_idx,
+                    elem_type,
+                )
+                continue
+
+            vtk_conductor = points_to_vtkpoly(elem_points)
+
+            if len(conductor.cross_section) > 0:
+                vtk_conductor = self._add_cross_section(
+                    vtk_conductor, conductor, elem_idx
+                )
+
+            conductor_block.AddInputData(vtk_conductor)
+        conductor_block.Update()
+        return conductor_block.GetOutput()
 
     def _pol_to_cart3d(self, point, idx):
         x, y = pol_to_cart(point.r[idx], point.phi[idx])
@@ -166,12 +160,6 @@ class CoilsNonAxisymmetricReader(GGDVTKPluginBase):
         p_start = self._pol_to_cart3d(elements.start_points, idx)
         p_end = self._pol_to_cart3d(elements.end_points, idx)
         return np.array([p_start, p_end])
-
-    def _create_arc_of_circle(self, elements, idx):
-        return self._create_circular_geometry(elements, idx, is_full_circle=False)
-
-    def _create_circle(self, elements, idx):
-        return self._create_circular_geometry(elements, idx, is_full_circle=True)
 
     def _create_circular_geometry(self, elements, idx, is_full_circle):
         p_start = self._pol_to_cart3d(elements.start_points, idx)
@@ -201,3 +189,36 @@ class CoilsNonAxisymmetricReader(GGDVTKPluginBase):
 
         t = np.linspace(0, max_angle, self.resolution)[:, np.newaxis]
         return p_centre + radius * (np.cos(t) * v_start + np.sin(t) * tangent)
+
+    def _add_cross_section(self, input_poly_line, conductor, elem_idx):
+        cross_section_idx = 0 if len(conductor.cross_section) == 1 else elem_idx
+        cross_section = conductor.cross_section[cross_section_idx]
+
+        if cross_section.geometry_type.index == 5:
+            return self._apply_annulus_filter(input_poly_line, cross_section)
+        else:
+            logger.warning(
+                "Cross-section %d is not supported, it will be represented as a line ",
+                cross_section.geometry_type.index,
+            )
+            return input_poly_line
+
+    def _apply_annulus_filter(self, poly_line, cross_section):
+        outer_tube = vtkTubeFilter()
+        outer_tube.SetInputData(poly_line)
+        outer_tube.SetRadius(cross_section.width / 2.0)
+        outer_tube.SetNumberOfSides(self.cs_resolution)
+        outer_tube.Update()
+
+        inner_tube = vtkTubeFilter()
+        inner_tube.SetInputData(poly_line)
+        inner_tube.SetRadius(cross_section.radius_inner)
+        inner_tube.SetNumberOfSides(self.cs_resolution)
+        inner_tube.Update()
+
+        append = vtkAppendPolyData()
+        append.AddInputData(outer_tube.GetOutput())
+        append.AddInputData(inner_tube.GetOutput())
+        append.Update()
+
+        return append.GetOutput()
